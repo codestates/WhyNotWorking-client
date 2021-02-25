85a7240789955adb4eb1bb17c05752af873957ea
MUNA <munawiki@gmail.com>
2021-02-25 10:44:36 +0900

    css 초기화

---
+++ src/features/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Nav.module.css';
+
+export function Nav() {
+
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+}
