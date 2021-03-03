6ad6d69534580ba29dd1c0e385895f3fa82ff7c8
MUNA <munawiki@gmail.com>
2021-03-04 00:57:34 +0900

    route install & implement

--- src/features/questionList/QuestionList.tsx
+++ src/features/questionList/QuestionList.tsx
@@ -4,8 +4,12 @@ import styles from "./questionList.module.css";
 import { Post } from "../post/Post";
 import { faCaretDown, faCog } from "@fortawesome/free-solid-svg-icons";
 import { Pagination } from "../pagination/Pagination";
+import { useRouteMatch } from "react-router-dom";
 
 export function QuestionList() {
+  let match = useRouteMatch();
+
+  console.log(match);
   return (
     <div className={styles.container}>
       <div className={styles.questionBox}>
--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import styles from "./Sidebar.module.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 export function Sidebar() {
+  let match = useRouteMatch();
+
+  console.log(match);
+
   return (
     <div className={styles.container}>
       <ul className={styles.content}>
-        <li className={`${styles.menu} ${styles.clicked}`}>Home</li>
+        <Link to="/">
+          <li className={`${styles.menu} ${styles.clicked}`}>HOME</li>
+        </Link>
         <ul className={styles.inner_menu}>
           <li className={styles.title}>PUBLIC</li>
-          <li className={styles.menu}>Stack Overflow</li>
-          <li className={styles.menu}>Tags</li>
+          <Link to="/questions">
+            <li className={styles.menu}>Stack Overflow</li>
+          </Link>
+          <Link to="/tags">
+            <li className={styles.menu}>Tags</li>
+          </Link>
           <li className={styles.menu}>Users</li>
         </ul>
         <ul className={styles.inner_menu}>
