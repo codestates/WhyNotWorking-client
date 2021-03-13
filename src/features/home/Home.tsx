d6749afdc888147cc3742fb6b754996bee3fb78a
ssdd33 <ojjonino@ojjoninoui-MacBookPro.local>
2021-03-14 01:41:07 +0900

    landing page 추가

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useRouteMatch, useHistory } from "react-router-dom";
+import { Description } from "../description/Description";
 import { setCurrentPage } from "../sidebar/sidebarSlice";
 import { gitHubSignUp } from "../signUp/signUpSlice";
 import styles from "./Home.module.css";
@@ -22,5 +23,13 @@ export function Home() {
     dispatch(setCurrentPage(match.path));
   }, [dispatch, match]);
 
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}></div>
+      <div className={styles.head}>
+        Questions are everywhere, answers are on Queue Overflow
+      </div>
+      <Description />
+    </div>
+  );
 }
